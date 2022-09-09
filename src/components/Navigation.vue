7d191fb8b6c9caf70bf2af2da3bc5816dfbdd160
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-09-09 11:51:48 +0300

    Change texts
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -19,7 +19,6 @@ const props = defineProps({
       <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSdkxyU_Z_Ve43vU4RaYOHMIRPfDcSILmgS1se7BiAZIxY46SA/viewform?usp=sf_link" rel="nofollow noopener" target="_blank">Add place</a></p>
       <p><a href="https://t.me/WorkplacesCy" rel="nofollow noopener" target="_blank"><img height="20" src="/src/assets/icons/telegram.svg" width="20" alt="Telegram group"></a> <a href="https://t.me/WorkplacesCy" rel="nofollow noopener" target="_blank">Telegram group</a></p>
       <p><a href="https://ko-fi.com/vladimir_mvs" rel="nofollow noopener" target="_blank"><img height="20" src="/src/assets/icons/ko-fi.svg" width="20" alt="Support Me"></a> <a href="https://ko-fi.com/vladimir_mvs" rel="nofollow noopener" target="_blank">Support Me</a></p>
-      <p>Don't forget to buy a coffee!</p>
     </v-card-text>
   </v-card>
 </template>
