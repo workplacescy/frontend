55e8147ddb0ab5ccbc681a493afbf4f84fd75ba7
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-09-30 17:10:20 +0300

    Use small image for preloading
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- src/components/PlacesItemPhotos.vue
+++ src/components/PlacesItemPhotos.vue
@@ -20,7 +20,7 @@ const modules = [Navigation]
 <template>
   <swiper :modules="modules" :navigation="{disabledClass: 'd-none', prevEl: '.navigation-prev', nextEl: '.navigation-next'}" :preload-images="false" :space-between="8" grab-cursor slides-per-view="2.5" @mouseenter="navigationVisible = true" @mouseleave="navigationVisible = false">
     <swiper-slide v-for="(photo, index) in props.photos" :key="photo.thumb.src">
-      <v-img :alt="photo.thumb.alt" :lazy-src="photo.thumb.src" :src="photo.thumb.src" :title="photo.thumb.caption" :width="photo.thumb.width" aspect-ratio="1" cover="" @click="$emit('clickPhoto', index, $event)"/>
+      <v-img :alt="photo.thumb.alt" lazy-src="/src/assets/images/image-area.svg" :src="photo.thumb.src" :title="photo.thumb.caption" :width="photo.thumb.width" aspect-ratio="1" cover="" @click="$emit('clickPhoto', index, $event)"/>
     </swiper-slide>
     <v-fade-transition>
       <v-btn v-show="navigationVisible" :icon="mdiChevronLeft" aria-label="Prev" class="navigation-prev" elevation="1" location="left center" position="absolute" size="small" style="left: 0.5rem; z-index: 1000" title="Prev"/>
