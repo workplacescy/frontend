51674845505b789e529334d55c6620ee300770b6
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-10-26 18:55:28 +0300

    Remove link to Telegram group
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -13,7 +13,6 @@ const props = defineProps({
     <v-card-text>
       <p><a href="/about/">About</a></p>
       <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSdkxyU_Z_Ve43vU4RaYOHMIRPfDcSILmgS1se7BiAZIxY46SA/viewform?usp=sf_link" rel="nofollow noopener" target="_blank">Add place</a></p>
-      <p><a href="https://t.me/WorkplacesCy" rel="nofollow noopener" target="_blank"><img height="20" src="/src/assets/icons/telegram.svg" width="20" alt="Telegram group"></a> <a href="https://t.me/WorkplacesCy" rel="nofollow noopener" target="_blank">Telegram group</a></p>
       <p><a href="https://ko-fi.com/vladimir_mvs" rel="nofollow noopener" target="_blank"><img height="20" src="/src/assets/icons/ko-fi.svg" width="20" alt="Support Me"></a> <a href="https://ko-fi.com/vladimir_mvs" rel="nofollow noopener" target="_blank">Support Me</a></p>
     </v-card-text>
   </v-card>
