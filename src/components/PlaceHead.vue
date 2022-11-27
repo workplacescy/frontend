6f18475193e9e552c6f4b67635921fdcab79e9eb
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-11-27 21:36:19 +0200

    SEO
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- src/components/PlaceHead.vue
+++ src/components/PlaceHead.vue
@@ -18,7 +18,7 @@ useHead({
       name: 'description',
       content: computed(() => props.place
           ? `${props.place.type} ${props.place.title}. ${props.place.address}. Rating ${props.place.vRate}. ${(props.place.description ? props.place.description + '. ' : '')} ${humanize.sockets(props.place.sockets)}, ${humanize.noise(props.place.noise)}, ${humanize.size(props.place.size)}, ${humanize.busyness(props.place.busyness)}, ${humanize.view(props.place.view)}, ${humanize.cuisine(props.place.cuisine)}${(props.place.wifi.name ? '. Wi-Fi name ' + props.place.wifi.name : '')}${(props.place.wifi.password ? ', Wi-Fi password ' + props.place.wifi.password : '')}`
-          : 'Map and list of workplaces (coffeeshops, cafes and coworkings) in Cyprus: Nicosia, Limassol, Larnaca, Paphos. With amenities, features, rating and Wi-Fi passwords. For digital nomads and freelancers'
+          : 'Map and list of workplaces (coffee shops, cafes and coworkings with sockets) in Cyprus: Nicosia, Limassol, Larnaca, Paphos. With amenities, features, rating and Wi-Fi passwords. For digital nomads and freelancers'
       )
     },
   ],
