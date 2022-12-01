2275285bfd9c41ab801ea603c89f9f7e9942968f
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-12-01 12:01:57 +0200

    Add canonical link
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- src/components/PlaceHead.vue
+++ src/components/PlaceHead.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {computed} from "vue";
 import {useHead} from "@vueuse/head";
+import {useRouter} from "vue-router";
 import {humanize} from "../composable/humanize.js";
 
 const props = defineProps({
@@ -10,21 +10,29 @@ const props = defineProps({
   }
 })
 
+const router = useRouter()
+
 useHead({
-  title: computed(() => props.place?.title),
-  titleTemplate: computed(() => props.place
-      ? '%s | Workplaces in Cyprus'
-      : null
-  ),
+  link: [
+    {
+      rel: 'canonical',
+      href: () => props.place
+          ? router.resolve({name: 'place', params: {id: props.place.id}}).href
+          : '/'
+    },
+  ],
   meta: [
     {
       name: 'description',
-      content: computed(() => props.place
+      content: () => props.place
           ? `${props.place.type} ${props.place.title}. ${props.place.address}. Rating ${props.place.vRate}. ${(props.place.description ? props.place.description + '. ' : '')} ${humanize.sockets(props.place.sockets)}, ${humanize.noise(props.place.noise)}, ${humanize.size(props.place.size)}, ${humanize.busyness(props.place.busyness)}, ${humanize.view(props.place.view)}, ${humanize.cuisine(props.place.cuisine)}${(props.place.wifi.name ? '. Wi-Fi name ' + props.place.wifi.name : '')}${(props.place.wifi.password ? ', Wi-Fi password ' + props.place.wifi.password : '')}`
           : 'Map and list of workplaces (coffee shops, cafes and coworkings with sockets) in Cyprus: Nicosia, Limassol, Larnaca, Paphos. With amenities, features, rating and Wi-Fi passwords. For digital nomads and freelancers'
-      )
     },
   ],
+  title: () => props.place?.title,
+  titleTemplate: (title) => title
+      ? `${title} | Workplaces in Cyprus`
+      : null,
 })
 </script>
 
