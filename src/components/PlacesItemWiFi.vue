d4a3773480c78d2dcb2936d48c2731d7de74ea2f
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-12-03 18:50:48 +0200

    Add Wi-Fi icon
    Signed-off-by: Vladimir Martsul <vladimir.martsul@gmail.com>

--- src/components/PlacesItemWiFi.vue
+++ src/components/PlacesItemWiFi.vue
@@ -1,6 +1,6 @@
 <script setup>
 import {useClipboard} from '@vueuse/core'
-import {mdiContentCopy} from '@mdi/js';
+import {mdiContentCopy, mdiWifi} from '@mdi/js';
 
 const props = defineProps({
   place: Object,
@@ -11,7 +11,7 @@ const {copy} = useClipboard()
 
 <template>
   <span v-if="props.place.wifi.name !== null" title="Click to copy password" @click.prevent.stop="copy(props.place.wifi.password)">
-    Wi-Fi:
+    <v-icon :icon="mdiWifi" aria-hidden="false" aria-label="Wi-Fi" class="mr-1" color="secondary" role="img" size="x-small"/>
     <code>{{ props.place.wifi.name }}</code>
     <span v-if="props.place.wifi.password !== null"> / <code>{{ props.place.wifi.password }}</code></span>
     <v-btn :icon="mdiContentCopy" aria-label="Click to copy password" color="primary" density="comfortable" size="x-small" variant="plain" @click.prevent=""/>
